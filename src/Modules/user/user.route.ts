c196ae1394fc2ab4ba66aa6500b1ce25d1201a4a
RaselShah <mdraselshah56@gmail.com>
2023-05-13 02:30:09 +0600

    oop patern admin find statics method create and instance method

--- src/Modules/user/user.route.ts
+++ src/Modules/user/user.route.ts
@@ -7,7 +7,7 @@ import {
 } from "./user.controller";
 
 const router = express.Router();
-// alwayes id param route bottom position set otherwise its call first dynamic id route
+// alwayes id param route bottom position set otherwise its call first dynamic id route -> alwayes static route first call then dynamic route call
 router.get("/", getUsers);
 router.get("/admins", getAdminUsers);
 router.get("/:id", getUserById);
