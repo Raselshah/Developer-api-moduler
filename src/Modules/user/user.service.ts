006761eefb9dba6c112fed541ceab60ba0395b1a
RaselShah <mdraselshah56@gmail.com>
2023-05-13 00:56:02 +0600

    filter by find name

--- src/Modules/user/user.service.ts
+++ src/Modules/user/user.service.ts
@@ -30,6 +30,6 @@ export const getUsersFromDB = async (): Promise<IUser[]> => {
 export const getUserByIdFromDB = async (
   payload: string
 ): Promise<IUser | null> => {
-  const user = await User.findOne({ id: payload });
+  const user = await User.findOne({ id: payload }, { name: 1, contactNo: 1 });
   return user;
 };
